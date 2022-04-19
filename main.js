b9692697bd1a17c2617e3f1dbc5a5abb4d691bdc
andro2ss <a.brzeski@hotmail.com>
2022-04-19 16:43:46 +0200

    progress backup

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 import './main.scss';
+
+const formularz = document.getElementById('formularz');
+const nameField = document.getElementById('formName');
+const emailField = document.getElementById('formEmail');
+const phoneField = document.getElementById('formPhone');
+
+
+
+function handleFormName() {
+  console.log(nameField.value);
+  if (nameField.value.length <= 3) {
+    nameField.classList.add('error__input');
+  } else if (nameField.classList.contains('error__input')) {
+    nameField.classList.remove('error__input');
+  }
+}
+
+function handleFormOnSubmit(e) {
+  e.preventDefault();
+  handleFormName();
+  console.log(nameField.value);
+}
+
+formularz.addEventListener('submit', handleFormOnSubmit);
